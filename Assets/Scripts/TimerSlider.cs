48353f2d69a21574417723e9c32d601be87d8790
krrn0722 <mur0722@outlook.jp>
2024-11-06 16:10:49 +0900

    タイマーを表すゲージを作成 maxvalue秒分のゲージ

---
+++ Assets/Scripts/TimerSlider.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerSlider : MonoBehaviour
+{
+    bool isStartTimer = false;
+    public Slider timerSlider;
+    public Image FillImage;
+    float totalTime;
+    float reduceValue;
+    float colorChangeTime;
+
+    void Start()
+    {
+        // sliderのmaxValue秒ゲームをする
+        totalTime = timerSlider.maxValue;
+        timerSlider.value = totalTime;
+        colorChangeTime = totalTime / 3;
+    }
+
+    void Update()
+    {
+        if(isStartTimer)
+        {
+            timerSlider.value -= Time.deltaTime;
+            if(timerSlider.value <= colorChangeTime)
+            {
+                FillImage.color = new Color32(255,0,0,255);;
+            }
+            if(timerSlider.value <= 0)
+            {
+                Debug.Log("Time Up!");
+                isStartTimer = false;
+            }
+        }
+        
+    }
+    public void onTimerStart()
+    {
+        isStartTimer = true;
+    }
+}
