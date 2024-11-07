616d9d2f6d9dc04d65310706e6ca5a59680bf099
Kobayashi Yuhi <meikentarocheese@gmail.com>
2024-11-07 20:01:52 +0900

    集めた葉っぱがくっつくようにした

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,19 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Basket : MonoBehaviour
 {
+    [SerializeField] GameObject basket; // 籠のオブジェクト
     public int score = 0; // 現在のスコア
     public int scoreValue = 10; // 加算/減算されるスコアの値
+
+    private GameObject obj;
+    private Rigidbody rb;
+    private BoxCollider bocol;
+
      // お茶の葉が籠に入ったとき
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("TeaLeaf"))
         {
-            // 接触したオブジェクトの名前をコンソールに表示
-            Debug.Log(other.gameObject.name + "がトリガーに入りました。");  
-            score += scoreValue;
+            obj = other.gameObject;
+            rb = obj.GetComponent<Rigidbody>();
+            if (rb.isKinematic == false)
+            {
+                
+                Debug.Log(other.gameObject.name + "がトリガーに入りました。");  
+                StartCoroutine(DelayCoroutine(0.4f, () => {
+                    bocol = obj.GetComponent<BoxCollider>();
+                    bocol.enabled = false;
+                    rb.isKinematic = true;
+                    obj.transform.parent = basket.transform;
+                    score += scoreValue;
+                }));
+            }
+            // 接触したオブジェクトの名前をコンソールに表示            
             Debug.Log("Tea Leaf entered! Score: " + score);
         }
+    }
+    private IEnumerator DelayCoroutine(float seconds, UnityAction callback)
+    {
+        yield return new WaitForSeconds(seconds);
+        callback?.Invoke();
     }
 
     // お茶の葉が籠から出たとき
